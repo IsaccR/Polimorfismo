a1885e9c3b62375f9a11675e14acf9b7603abd05
Your Name <youexample.com>
2026-04-23 15:45:40 -0600

    Ejercicio 2

--- Polimorfismo/Main.java
+++ Polimorfismo/Main.java
@@ -1,17 +1,23 @@
 package Polimorfismo;
 
 
-import Polimorfismo.ejercicio01.*; 
+import Polimorfismo.ejercicio01.*;
+import Polimorfismo.ejercicio02.*;
 
 
 
 public class Main {
  public static void main(String[] args) {
      
-     System.out.println(" EJERCICIO 1 ");
+     //Ejercicio 1 Animal 
+	 System.out.println(" EJERCICIO 1 ");
      Animal miAnimal = new Perro(); 
      miAnimal.hacerSonido();
 
-   
+     //Ejercicio 2 Figura 
+     System.out.println( "\n Ejercicio 2:");
+ 
+  Figura miFigura = new Rectangulo(10.5, 5.0); 
+  miFigura.area(); 
  }
 }
---
+++ Polimorfismo/ejercicio02/Figura.java
@@ -0,0 +1,8 @@
+package Polimorfismo.ejercicio02;
+
+public class Figura {
+    
+    public void area() {
+        System.out.println("Esta es una figura genérica, el área no está definida.");
+    }
+}
